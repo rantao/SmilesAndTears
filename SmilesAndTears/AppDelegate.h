81f6fd1aee4969db88ce5466c1832be720d410d5
Ran Tao <ran@ephexi.com>
2012-08-21 11:16:33 -0700

    Initial Commit

---
+++ SmilesAndTears/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SmilesAndTears
+//
+//  Created by Ran Tao on 8.21.12.
+//  Copyright (c) 2012 Ran Tao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SmilesAndTears/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SmilesAndTears
+//
+//  Created by Ran Tao on 8.21.12.
+//  Copyright (c) 2012 Ran Tao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
